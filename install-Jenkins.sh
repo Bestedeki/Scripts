8eac320b471d21de44caec5411e24b4e19e01f20
Bestedeki <132979090+Bestedeki@users.noreply.github.com>
2024-03-29 15:49:45 -0600

    Update install-Jenkins.sh

--- install-Jenkins.sh
+++ install-Jenkins.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# Install OpenJDK 17 JRE Headless
-sudo apt install openjdk-11-jre -y
+sudo apt-get install openjdk-11-jre -y
 # Download Jenkins GPG key
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 # Add Jenkins repository to package manager sources
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 # Update package manager repositories
@@ -11,8 +10,8 @@ sudo apt-get update
 sudo apt-get install jenkins -y
 # Login to Jenkins UI
 # Install docker pipeline
-sudo apt update
-sudo apt install docker.io
+sudo apt-get update
+sudo apt-get install docker.io
 sudo su - 
 usermod -aG docker jenkins
 usermod -aG docker ubuntu
